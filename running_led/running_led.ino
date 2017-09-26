4195169fd46f82a06053342fc62f4b5b194fabdc
valerianmp <32305982+valerianmp@users.noreply.github.com>
2017-09-26 23:52:26 +0700

    Dokumentasi kode sumber
    Penambahan komentar pada kode sumber untuk mempermudah kode dibaca

--- running_led/running_led.ino
+++ running_led/running_led.ino
@@ -1,24 +1,24 @@
 void setup() {
   // put your setup code here, to run once:
   for(int i = 8; i<=12; i++){
-    pinMode(i, OUTPUT);
+    pinMode(i, OUTPUT); //set every pin from 8 - 12 to be an output pin
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int i;
-  for(i = 8; i <= 12; i++){
-    for(int j = 0; j<= 255; j+=3){
+  for(i = 8; i <= 12; i++){   //for each led
+    for(int j = 0; j<= 255; j+=3){  //fade in
       analogWrite(i,j);
       delay(1);
     }
     delay(50);
-    for(int j = 255; j>=0; j-=3){
+    for(int j = 255; j>=0; j-=3){ //and then fade out so we can move to the next led
       analogWrite(i,j);
       delay(1);
     }
     delay(50);
   }
-  delay(800);
+  delay(300);   //delay to smooth out the movement
 }
